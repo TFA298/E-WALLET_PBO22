63bcc25cd02daf60f98485e7ad9bd4f1d73fddf4
TFA298 <hyperionfubuki@gmail.com>
2023-01-20 08:18:37 +0700

    Progress modul 2

---
+++ src/App.java
@@ -0,0 +1,27 @@
+class Akun{
+    int id;
+    int pin;
+    char username;
+    char password;
+}
+
+class Transaksi{
+    double nominal;
+    char provider;
+    char history;
+}
+
+class Topup{
+    double nominal_topup;
+    char aplikasi;
+}
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
+
+
+
+
+
